9f6fa7b44204aadb6f45384df4a995b6bea15d82
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-22 02:28:09 +0330

    Feat: get user number

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,12 @@
 "use strict";
-// get check button
+// select check button
 const check = document.querySelector(".check");
+// compare user number - function
+const compareUserNumber = () => {
+    // get user number
+    const userNumber = +document.querySelector(".user-input").value;
+    if (!userNumber) {
+        console.log("It's Zero");
+    }
+};
+check.addEventListener("click", compareUserNumber);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
-// get check button
-const check = document.querySelector(".check") ! as HTMLButtonElement;
+// select check button
+const check = document.querySelector(".check")! as HTMLButtonElement;
 
-
+// compare user number - function
+const compareUserNumber = () => {
+  // get user number
+  const userNumber = +(document.querySelector(".user-input") ! as HTMLInputElement).value;
+};
+check.addEventListener("click", compareUserNumber);
